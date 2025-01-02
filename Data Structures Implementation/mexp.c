ab7abf4bd4f4c242dde0f95890ef468386d58c0e
mariammkkk <mariamkhan8805@gmail.com>
2025-01-01 21:14:59 -0500

    commit

--- Data Structures Implementation/mexp.c
+++
@@ -1,8 +0,0 @@
-void mexp(int* k) {
-
-    // k x k size of matrix M
-    // traversing matrix M (k rows, k columns)
-    // n (exponent) - number of times to multiply the matrix by itself (M^n)
-
-
-}
